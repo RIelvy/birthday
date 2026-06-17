514160f99b085b0d9e6c63c23b1ee4fa0f64bae6
Dika Aditya Naufaldi <dikanaufaldi220@gmail.com>
2026-06-17 15:58:20 +0700

    fix github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Letter from "./pages/Letter"; 
+import Letter from "./pages/Letter";
 import TheBirthdayLetter from "./pages/TheBirthdayLetter";
 import "./App.css";
 
